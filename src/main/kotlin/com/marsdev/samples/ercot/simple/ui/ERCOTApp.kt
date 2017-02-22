eb6b58cbd11cd4a0ee991805257408cd507a7b9b
jchildress <john@mars-dev.com>
2017-02-22 07:57:40 -0600

    formatting...

--- src/main/kotlin/com/marsdev/samples/ercot/simple/ui/ERCOTApp.kt
+++ src/main/kotlin/com/marsdev/samples/ercot/simple/ui/ERCOTApp.kt
@@ -3,8 +3,10 @@ package com.marsdev.samples.ercot.simple.ui
 import com.marsdev.samples.ercot.simple.common.ERCOTNode
 import com.marsdev.samples.ercot.simple.common.SPPValue
 import javafx.collections.FXCollections
+import javafx.geometry.Pos
 import javafx.scene.chart.CategoryAxis
 import javafx.scene.chart.NumberAxis
+import javafx.scene.text.FontWeight
 import tornadofx.*
 import java.time.LocalDate
 
@@ -49,7 +51,14 @@ class ERCOTNodeList : View("ERCOT Nodes") {
             listview<SPPValue> {
                 items = controller.settlementPointPrices
                 cellCache {
-                    label(it.hourEnding.toString() + ":  " + it.settlementPointPrice.toString())
+                    //                    label(it.hourEnding.toString() + ":  " + it.settlementPointPrice.toString())
+                    label("Hour Ending ${it.hourEnding + 1}:  $${it.settlementPointPrice}") {
+                        alignment = Pos.CENTER_RIGHT
+                        style {
+                            fontSize = 16.px
+                            fontWeight = FontWeight.MEDIUM
+                        }
+                    }
                 }
                 selectionModel.selectedItemProperty().onChange {
                     // todo not working
